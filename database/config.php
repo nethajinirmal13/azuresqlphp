87760f309556765e9e1cdb1fdb8b5cbe9fe92205
nethaji nirmal <nethajinirmal13@gmail.com>
2022-07-16 10:34:15 +0530

    Update config.php

--- database/config.php
+++ database/config.php
@@ -3,7 +3,7 @@
 // Configuration for database connection
 
 $host       = getenv('DB_HOST');
-$username   = getenv('DB_USERNAME');
-$password   = getenv('DB_PASSWORD');
+$username   = getenv('allana');
+$password   = getenv('Sample@13');
 $db_name     = getenv('DB_DATABASE');
 $sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
